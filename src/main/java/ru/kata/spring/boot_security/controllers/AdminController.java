ad49c0f46eaefb3b7d4a2820d66d274419091bb5
sakhnopy <daniilsahnov@mail.ru>
2022-11-11 03:57:38 +0300

    done

--- src/main/java/ru/kata/spring/boot_security/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/controllers/AdminController.java
@@ -2,10 +2,6 @@ package ru.kata.spring.boot_security.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.model.Role;
 import ru.kata.spring.boot_security.model.User;
